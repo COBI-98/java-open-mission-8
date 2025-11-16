c3e12dd8f1289cd6a154722ccaa10301d185c556
COBI-98 <tkdgus968@naver.com>
2025-11-16 12:04:00 +0900

    feat(requiredTask): 항공 서비스 기본 소요시간을 할당

--- src/main/java/airport/domain/user/RequiredAirportTask.java
+++ src/main/java/airport/domain/user/RequiredAirportTask.java
@@ -1,18 +1,21 @@
 package airport.domain.user;
 
+import airport.domain.calendar.DayType;
 import java.util.Arrays;
 
 public enum RequiredAirportTask {
 
-    USIM("유심/eSIM 구매"),
-    EXCHANGE("환전"),
-    TRANSPORT_CARD("교통카드 충전/구매"),
-    MEAL("식사"),
-    SHOWER("샤워");
+    USIM(15, "USIM/eSIM 구매"),
+    EXCHANGE(10, "환전"),
+    TRANSPORT_CARD(10, "교통카드 충전"),
+    MEAL(20, "간단한 식사"),
+    SHOWER(25, "샤워");
 
+    private final int baseMinutes;
     private final String description;
 
-    RequiredAirportTask(String description) {
+    RequiredAirportTask(final int baseMinutes, final String description) {
+        this.baseMinutes = baseMinutes;
         this.description = description;
     }
 
@@ -37,5 +40,9 @@ public enum RequiredAirportTask {
 
     public String getDescription() {
         return description;
+    }
+
+    public int durationMinutes(final DayType dayType) {
+        return (int) Math.round(baseMinutes * dayType.taskDelayFactor());
     }
 }
